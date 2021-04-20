5bf588f9108f1e93aeecef64a65f52a2e5b79e16
cherpeco <cherpeco@gmail.com>
2021-04-20 23:42:41 +0300

    Popular item ( without ajax)

---
+++ dist/index.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var cart_item = parseInt(document.getElementById("car-item").textContent, 10);
+var whislist = parseInt(document.getElementById("whish-item").textContent, 10);
+
+function addcart() {
+  cart_item++;
+  document.getElementById('car-item').innerHTML = cart_item;
+}
+
+function addwhishlist() {
+  whislist++;
+  document.getElementById('whish-item').innerHTML = whislist;
+}
---
+++ index.js
@@ -0,0 +1,12 @@
+var cart_item =parseInt(document.getElementById("car-item").textContent, 10);
+var whislist = parseInt(document.getElementById("whish-item").textContent, 10);
+function addcart () {
+cart_item++;
+ document.getElementById('car-item').innerHTML= cart_item;
+}
+
+function addwhishlist(){
+  whislist++;
+  document.getElementById('whish-item').innerHTML= whislist;
+
+}
