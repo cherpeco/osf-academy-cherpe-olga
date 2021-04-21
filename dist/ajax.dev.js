4fb5db62b420e88012a3c26e039e4662042fdb7a
cherpeco <cherpeco@gmail.com>
2021-04-22 00:30:09 +0300

    Load more from JSON file

--- dist/ajax.dev.js
+++ dist/ajax.dev.js
@@ -1,10 +1,19 @@
 "use strict";
 
 $(document).ready(function () {
-  $.getJSON("./images.json", function (data) {
-    console.log("Hello");
-    console.log("DATA" + data.description); // Prints: Harry
-  }).fail(function () {
-    console.log("An error has occurred.");
+  $(document).on('click', '#load-more', function (e) {
+    $.ajax({
+      type: 'GET',
+      url: './images.json',
+      dataType: 'json',
+      success: function success(data) {
+        var newdata = "";
+        newdata += "<div class='row-item col-12'> <div class='col-3 product_item'><img src='" + data.product[0].src + " '/> " + "<br>" + "<p>" + data.product[0].name + " <br>" + data.product[0].price + "</p> </div>" + "<div class='col-3 product_item'><img src='" + data.product[1].src + " '/> " + "<br>" + "<p>" + data.product[1].name + " <br>" + data.product[1].price + "</p> </div>" + "<div class='col-3 product_item'><img src='" + data.product[2].src + " '/> " + "<br>" + "<p>" + data.product[2].name + " <br>" + data.product[2].price + "</p> </div>" + "<div class='col-3 product_item'><img src='" + data.product[3].src + " '/> " + "<br>" + "<p>" + data.product[3].name + " <br>" + data.product[3].price + "</p> </div> </div";
+        $('#product').append(newdata);
+      },
+      error: function error() {
+        console.log('Houston, we have a problem!');
+      }
+    });
   });
 });
