b911de8336b37ab0b7084f6e179dc24fa58a5d7c
cherpeco <cherpeco@gmail.com>
2021-04-23 18:43:14 +0300

    Popular items on mobile version

--- dist/ajax.dev.js
+++ dist/ajax.dev.js
@@ -7,8 +7,14 @@ $(document).ready(function () {
       url: './images.json',
       dataType: 'json',
       success: function success(data) {
-        var newdata = "";
-        newdata += "<div class='row-item col-12'> <div class='col-3 product_item'><img src='" + data.product[0].src + " '/> " + "<br>" + "<p>" + data.product[0].name + " <br>" + data.product[0].price + "</p> </div>" + "<div class='col-3 product_item'><img src='" + data.product[1].src + " '/> " + "<br>" + "<p>" + data.product[1].name + " <br>" + data.product[1].price + "</p> </div>" + "<div class='col-3 product_item'><img src='" + data.product[2].src + " '/> " + "<br>" + "<p>" + data.product[2].name + " <br>" + data.product[2].price + "</p> </div>" + "<div class='col-3 product_item'><img src='" + data.product[3].src + " '/> " + "<br>" + "<p>" + data.product[3].name + " <br>" + data.product[3].price + "</p> </div> </div";
+        if (window.matchMedia("(max-width: 900px)").matches) {
+          var newdata = "";
+          newdata += " <div class='col-12 col-md-3 product_item'  style='display:none' ><img src='" + data.product[0].src + " '/> " + "<br>" + "<p>" + data.product[0].name + " <br>" + data.product[0].price + "</p> </div>" + "<div class='col-12 col-md-3 product_item' style='display:none'><img src='" + data.product[1].src + " '/> " + "<br>" + "<p>" + data.product[1].name + " <br>" + data.product[1].price + "</p> </div>";
+        } else {
+          var newdata = "";
+          newdata += "<div class='col-12 col-md-3 product_item'><img src='" + data.product[0].src + " '/> " + "<br>" + "<p>" + data.product[0].name + " <br>" + data.product[0].price + "</p> </div>" + "<div class='col-12 col-md-3 product_item'><img src='" + data.product[1].src + " '/> " + "<br>" + "<p>" + data.product[1].name + " <br>" + data.product[1].price + "</p> </div>" + "<div class='col-12 col-md-3 product_item'><img src='" + data.product[2].src + " '/> " + "<br>" + "<p>" + data.product[2].name + " <br>" + data.product[2].price + "</p> </div>" + "<div class='col-12 col-md-3 product_item'><img src='" + data.product[3].src + " '/> " + "<br>" + "<p>" + data.product[3].name + " <br>" + data.product[3].price + "</p> </div> ";
+        }
+
         $('#product').append(newdata);
       },
       error: function error() {
--- index.js
+++ index.js
@@ -12,4 +12,45 @@ function addwhishlist(){
 }
 
 
+var slideIndex = 1;
+window.addEventListener("load",function() {
+ 
+  showSlides(slideIndex);
+ 
+})
+function plusSlides(n){
+
+  if (n < 0){
+    showSlides(slideIndex -= 1);
+  } else {
+   showSlides(slideIndex += 1); 
+  }
+}
+
+function showSlides(n){
+  var i;
+  var slides = document.getElementsByClassName("product_item");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    if (window.matchMedia("(max-width: 900px)").matches) {
+      slides[i].style.display = "none";
+    } else {
+      slides[slideIndex-1].style.display = "block";
+    }
+   
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+ 
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+function currentSlide(n){
+  showSlides(slideIndex = n);
+}
+
 
